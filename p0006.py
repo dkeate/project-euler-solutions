7d492b42f2d85137b0d7ee44055ff54b3465f8bf
Dustin Keate <dkeate@gmail.com>
2026-08-24 17:18:16 +0900

    Solved problem 6

---
+++ p0006.py
@@ -0,0 +1,24 @@
+# Sum Square Difference
+
+# The sum of the squares of the first ten natural numbers is, 1^2 + 2^2 + ... 10^2 = 385.
+
+# The square of the sum of the first ten natural numbers is, (1 + 2 + ... + 10)^2 = 55^2 = 3025.
+
+# Hence the difference between the sum of the squares of the first ten natural numbers and the
+# square of the sum is 3025 - 385 = 2640.
+
+# Find the difference between the sum of the squares of the first one hundred natural numbers and
+# the square of the sum.
+
+
+
+sum_of_sq = 0
+sq_of_sum = 0
+
+for i in range(1,101):
+    sum_of_sq = sum_of_sq + i**2
+    sq_of_sum = sq_of_sum + i
+
+sq_of_sum = sq_of_sum**2
+
+print (sq_of_sum - sum_of_sq)
